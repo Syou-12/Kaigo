cc064ed8593e2503fd7abcf5b4ac7d12ec72c9c7
Syou-12 <110447240+Syou-12@users.noreply.github.com>
2022-08-29 15:40:04 +0900

    Add files via upload
    修正

--- js/btn.js
+++ js/btn.js
@@ -4,4 +4,15 @@ $(function () {
         $('.btn').toggleClass('slide-in');
         $('body').toggleClass('noscroll');
     });
+
+$('.nav-list a').on('click',function() {
+    $('.btn').toggleClass('slide-in');
+    $('.bg-btn').toggleClass('close');
+    if($('.bg-btn').hasClass('active')) {
+        $('.nav-list a').addClass('active');
+    } else {
+        $('.nav-list a').removeClass('active');
+    }
+})
+
 });
